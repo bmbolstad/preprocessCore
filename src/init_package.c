7c4e68fe8c5aab4957e5d3bb1374e919aa5c7100
bolstad <bolstad@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-05-23 02:54:24 +0000

    add additional quantile normalization extensions that were previously in affyPLM
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/preprocessCore@24920 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/init_package.c
+++ src/init_package.c
@@ -15,13 +15,27 @@
 #include <R_ext/Rdynload.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
+#include <R_ext/RConverters.h>
+
+#if _MSC_VER >= 1000
+__declspec(dllexport)
+#endif
 
 
+static const R_CallMethodDef callMethods[]  = {
+  {"R_qnorm_c",(DL_FUNC)&R_qnorm_c,2},
+    {"R_qnorm_robust_weights", (DL_FUNC)&R_qnorm_robust_weights, 3},
+    {"R_qnorm_robust_c",(DL_FUNC)&R_qnorm_robust_c,6},
+    {"R_qnorm_determine_target",(DL_FUNC)&R_qnorm_determine_target,2},
+    {"R_qnorm_using_target",(DL_FUNC)&R_qnorm_using_target,3 },
+    {"R_qnorm_within_blocks",(DL_FUNC)&R_qnorm_within_blocks, 3},
+    {NULL, NULL, 0}
+  };
 
-void R_init_preprocessCore(){
+void R_init_preprocessCore(DllInfo *info){
 
-
+  R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+  
   R_RegisterCCallable("preprocessCore", "qnorm_c", (DL_FUNC)&qnorm_c);
-
-
+  R_RegisterCCallable("preprocessCore", "qnorm_robust_c", (DL_FUNC)&qnorm_robust_c);
 }
