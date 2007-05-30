fe82cdc260fd49150b7cc8f26649a078a7777973
bolstad <bolstad@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-05-30 01:42:15 +0000

    missing header file
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/preprocessCore@25035 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/include/rlm_se.h
@@ -0,0 +1,8 @@
+#ifndef RLM_SE_H
+#define RLM_SE_H 1
+
+
+void rlm_compute_se(double *X,double *Y, int n, int p, double *beta, double *resids,double *weights,double *se_estimates,double *varcov, double *residSE, int method,double (* PsiFn)(double, double, int), double psi_k);
+void rlm_compute_se_anova(double *Y, int y_rows,int y_cols, double *beta, double *resids,double *weights,double *se_estimates, double *varcov, double *residSE, int method,double (* PsiFn)(double, double, int), double psi_k);
+
+#endif
