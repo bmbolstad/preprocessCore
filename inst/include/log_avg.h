9cd7982f6596dadaeaf32ebae9475f25963d9959
Ben Bolstad <bolstad@stat.berkeley.edu>
2007-09-08 05:05:18 +0000

    add additional shared include files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/preprocessCore@26950 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/include/log_avg.h
@@ -0,0 +1,7 @@
+#ifndef LOG_AVG_H
+#define LOG_AVG_H 1
+
+void logaverage(double *data, int rows, int cols, double *results, double *resultsSE);
+void LogAverage(double *data, int rows, int cols, int *cur_rows, double *results, int nprobes, double *resultsSE);
+
+#endif
