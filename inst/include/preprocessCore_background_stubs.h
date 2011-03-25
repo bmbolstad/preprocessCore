38665aba9a113fde4eba1515a4ad3e7df0c9a9f4
Benilton Carvalho <bcarvalh@jhsph.edu>
2011-03-25 14:42:31 +0000

    Added preprocessCore_*_stubs.h
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/preprocessCore@54042 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/include/preprocessCore_background_stubs.h
@@ -0,0 +1,3 @@
+void rma_bg_parameters(double *PM,double *param, int rows, int cols, int column);
+void rma_bg_adjust(double *PM,double *param, int rows, int cols, int column);
+void rma_bg_correct(double *PM, int rows, int cols);
