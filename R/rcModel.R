7bc23dd934754a585974b8f1eedd2b278e6171d9
bolstad <bolstad@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-03-08 17:22:34 +0000

    (1.17.3) More implementation of subrcColModel routines
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/preprocessCore@63619 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rcModel.R
+++ R/rcModel.R
@@ -65,3 +65,61 @@ rcModelMedianPolish <- function(y){
   PsiK <- 1.345
   .Call("R_medianpolish_rma_default_model",y,PACKAGE="preprocessCore")
 }
+
+
+
+
+subrcModelMedianPolish <- function(y,group.labels){
+
+  if (!is.matrix(y))
+    stop("argument should be matrix")
+
+  if (!is.double(y) & is.numeric(y))
+    y <- matrix(as.double(y),dim(y)[1],dim(y)[2])
+  else if (!is.numeric(y))
+    stop("argument should be numeric matrix")
+
+  rowIndexList <- convert.group.labels(group.labels)
+  
+  x <- .Call("R_sub_rcModelSummarize_medianpolish", y, rowIndexList,PACKAGE="preprocessCore")
+
+  names(x) <- names(rowIndexList)
+  x
+}
+
+
+
+
+
+subrcModelPLM <- function(y,group.labels,row.effects=NULL, input.scale=NULL){
+
+  if (!is.matrix(y))
+    stop("argument should be matrix")  
+
+  if (!is.double(y) & is.numeric(y))
+    y <- matrix(as.double(y),dim(y)[1],dim(y)[2])
+  else if (!is.numeric(y))
+    stop("argument should be numeric matrix")
+   
+  rowIndexList <- convert.group.labels(group.labels)
+ 
+  PsiCode <- 0
+  PsiK <- 1.345
+
+  if (is.null(row.effects)){
+    x <- .Call("R_sub_rcModelSummarize_plm", y, rowIndexList, PsiCode, PsiK, input.scale,PACKAGE="preprocessCore")
+    names(x) <- names(rowIndexList)
+    x
+
+  } else {
+    stop("row.effects not yet implemented for subrcModelPLM")
+    if (length(row.effects) != nrow(y)){
+       stop("row.effects parameter should be same length as number of rows")
+    }  
+    if (abs(sum(row.effects)) > 10*.Machine$double.eps){
+       stop("row.effects should sum to zero")
+    }
+    .Call("R_rlm_rma_given_probe_effects",y,as.double(row.effects),PsiCode,PsiK,input.scale,PACKAGE="preprocessCore") 
+  }	
+}
+
