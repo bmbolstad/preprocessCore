b812698e02d70c0719b6647c5472fc4275f95c7a
Ben Bolstad <bmb@bmbolstad.com>
2014-09-01 17:11:40 -0700

    Code doc cleanup

--- src/log_avg.c
+++ src/log_avg.c
@@ -2,9 +2,9 @@
  **
  ** log_avg.c
  **
- ** Copyright (C) 2003-2007 Ben Bolstad
+ ** Copyright (C) 2003-2014 Ben Bolstad
  **
- ** created by: B. M. Bolstad   <bolstad@stat.berkeley.edu>
+ ** created by: B. M. Bolstad   <bmb@bmbolstad.com>
  ** created on: Feb 6, 2003  (but based on earlier work from Nov 2002)
  **
  ** last modified: Feb 6, 2003
@@ -13,13 +13,15 @@
  **
  ** General discussion
  **
- ** Implement log2 average pm summarization, with or without normalization
+ ** Implement log2 average pm summarization
  **
  ** Feb 6, 2002 - Initial version of this summarization method
  ** Jul 23, 2003 - parameter for storing SE added (not yet implemented)
  ** Oct 5, 2003 - method of adding parameters. 
  ** May 19, 2007 - branch out of affyPLM into a new package preprocessCore, then restructure the code. Add doxygen style documentation
  ** Sep 19, 2007 - add LogAverage_noSE
+ ** Sep, 2014 - change to size_t where appropriate. Clean up some of the code documentation
+ ** 
  **
  ************************************************************************/
 
@@ -66,11 +68,13 @@ static double LogAvg(double *x, size_t length){
   return (mean);    
 }
 
+
+
 /***************************************************************************
  ** 
- ** void averagelog(double *data, int rows, int cols, double *results, double *resultsSE)
+ ** void logaverage(double *data, int rows, int cols, double *results, double *resultsSE)
  **
- ** aim: given a data matrix of probe intensities, compute average of log2 values in column wise manner 
+ ** aim: given a data matrix of probe intensities, compute average of values, then log transform average, in column wise manner 
  **      
  **
  ** double *data - Probe intensity matrix
@@ -82,6 +86,20 @@ static double LogAvg(double *x, size_t length){
  **
  ***************************************************************************/
 
+/*! \brief compute the mean then log2 transform and also SE of the log2 mean
+ * 
+ *  Given a data matrix of probe intensities compute average expression measure then log2 transforms it and SE of this estimate
+ *  on a column by column basis
+ *    
+ *
+ * @param data a matrix containing data stored column-wise stored in rows*cols length of memory
+ * @param rows the number of rows in the matrix 
+ * @param cols the number of columns in the matrix
+ * @param results pre-allocated space to store output log2 averages. Should be of length cols
+ * @param resultsSE pre-allocated space to store SE of log2 averages. Should be of length cols
+ *
+ *  
+ */
 
 void logaverage(double *data, size_t rows, size_t cols, double *results, double *resultsSE){
 
@@ -96,7 +114,6 @@ void logaverage(double *data, size_t rows, size_t cols, double *results, double 
     resultsSE[j] = R_NaReal;
   } 
 }
-
 
 
 
@@ -149,6 +166,23 @@ void LogAverage(double *data, size_t rows, size_t cols, int *cur_rows, double *r
   Free(z);
 }
 
+
+
+/*! \brief compute the average and then log2 transform it.
+ *
+ * Given a data matrix of probe intensities, and a list of rows in the matrix 
+ *      corresponding to a single probeset, compute log2 average expression measure. 
+ *      Note that data is a probes by chips matrix. Also compute SE estimates
+ *
+ * @param data a matrix containing data stored column-wise stored in rows*cols length of memory
+ * @param rows the number of rows in the matrix 
+ * @param cols the number of columns in the matrix
+ * @param cur_rows a vector containing row indices to use
+ * @param results pre-allocated space to store output log2 averages. Should be of length cols
+ * @param nprobes number of probes in current set
+ *
+ *  
+ */
 
 void LogAverage_noSE(double *data, size_t rows, size_t cols, int *cur_rows, double *results, size_t nprobes){
 
