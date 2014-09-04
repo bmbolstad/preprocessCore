4ce27cd0581d5340757258539cfc33552595254e
Ben Bolstad <bmb@bmbolstad.com>
2014-09-03 19:15:20 -0700

    change kernel density estimation function interface

--- inst/include/weightedkerneldensity_stubs.c
+++ inst/include/weightedkerneldensity_stubs.c
@@ -4,13 +4,14 @@
 #ifndef WEIGHTEDKERNELDENSITY_STUBS_H
 #define WEIGHTEDKERNELDENSITY_STUBS_H 1
 
+#include <stddef.h>
 
-void KernelDensity(double *x, int *nxxx, double *weights, double *output, double *output_x, int *nout, int *kernel_fn, int *bandwidth_fn, double *bandwidth_adj){
+void KernelDensity(double *x, size_t nxxx, double *weights, double *output, double *output_x, size_t nout, int kernel_fn, int bandwidth_fn, double bandwidth_adj){
    
-  static void(*fun)(double*, int*,  double*, double*, double*, int *, int *, int *, double *) = NULL;
+  static void(*fun)(double*, size_t,  double*, double*, double*, size_t, int, int, double) = NULL;
   
   if (fun == NULL)
-    fun =  (void(*)(double*, int*,  double*, double*, double*, int *, int *, int *, double *))R_GetCCallable("preprocessCore","KernelDensity");
+    fun =  (void(*)(double*, size_t,  double*, double*, double*, size_t, int, int, double))R_GetCCallable("preprocessCore","KernelDensity");
   
   fun(x, nxxx, weights, output, output_x, nout, kernel_fn, bandwidth_fn, bandwidth_adj);
   
--- src/weightedkerneldensity.h
+++ src/weightedkerneldensity.h
@@ -1,7 +1,7 @@
 #ifndef WEIGHTEDKERNELDENSITY_H
 #define WEIGHTEDKERNELDENSITY_H
 
-void KernelDensity(double *x, int *nxxx, double *weights, double *output, double *output_x, int *nout, int *kernel_fn, int *bandwidth_fn, double *bandwidth_adj);
-void KernelDensity_lowmem(double *x, int *nxxx, double *output, double *output_x, int *nout);
+void KernelDensity(double *x, size_t nxxx, double *weights, double *output, double *output_x, size_t nout, int kernel_fn, int bandwidth_fn, double bandwidth_adj);
+void KernelDensity_lowmem(double *x, size_t nxxx, double *output, double *output_x, size_t nout);
 
 #endif
